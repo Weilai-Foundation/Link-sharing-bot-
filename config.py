1d41a58d14e5f72d26d81a7f8c939c5c77ff85dd
NamikazeBots <minatonamikaze.void@gmail.com>
2026-02-19 22:38:01 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ API_HASH = os.getenv("API_HASH", "a69e8b16dac958f1bd31eee360ec53fa")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8276153107:AAHV1SF_rg9OtrXARRF7yOKHBZWmykQ-dko")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Fugui:tianlu@cluster0.6hyrp0r.mongodb.net/?appName=Cluster0")
 ADMINS = [int(x) for x in os.getenv("ADMINS", "7840980054").split(",") if x.strip()]
-START_PIC = os.getenv("START_PIC", "")
-LINK_PIC = os.getenv("LINK_PIC", "")
+START_PIC = os.getenv("START_PIC", "https://i.ibb.co/nM9Ypvmq/jsorg.jpg")
+LINK_PIC = os.getenv("LINK_PIC", "https://i.ibb.co/nM9Ypvmq/jsorg.jpg")
