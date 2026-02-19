1348f4b2a7e570f6d9791e21bbd945d829458414
NamikazeBots <minatonamikaze.void@gmail.com>
2026-02-19 22:26:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x.strip()]
+API_ID = int(os.getenv("API_ID", "22884130"))
+API_HASH = os.getenv("API_HASH", "a69e8b16dac958f1bd31eee360ec53fa")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8276153107:AAHV1SF_rg9OtrXARRF7yOKHBZWmykQ-dko")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Fugui:tianlu@cluster0.6hyrp0r.mongodb.net/?appName=Cluster0")
+ADMINS = [int(x) for x in os.getenv("ADMINS", "7840980054").split(",") if x.strip()]
 START_PIC = os.getenv("START_PIC", "")
 LINK_PIC = os.getenv("LINK_PIC", "")
